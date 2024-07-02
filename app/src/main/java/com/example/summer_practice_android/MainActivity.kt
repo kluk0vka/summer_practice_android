d73357d82d1f672935f24f77c96df440c4d95fbb
leonteva.ekaterina <katya_leont@mail.ru>
2024-07-02 05:23:53 +0300

    дз 1

--- app/src/main/java/com/example/summer_practice_android/MainActivity.kt
+++ app/src/main/java/com/example/summer_practice_android/MainActivity.kt
@@ -1,20 +1,74 @@
 package com.example.summer_practice_android
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val carCountEditText = findViewById<EditText>(R.id.car_count)
+        val startRaceButton = findViewById<Button>(R.id.start_race_button)
+
+        startRaceButton.setOnClickListener {
+            val carCountText = carCountEditText.text.toString()
+            if (carCountText.isNotEmpty()) {
+                val carCount = carCountText.toInt()
+                val cars = raceParticipants(carCount)
+                startRace(cars)
+            }
         }
+    }
+
+    private fun raceParticipants(count: Int): List<Auto> {
+        val models = listOf("Camaro", "Corolla", "Camry", "Avalon", "Land Cruiser", "Mustang", "Wigo")
+        val brands = listOf("BMW", "Mercedes-Benz", "Toyota", "Chevrolet", "Honda", "Tesla", "Ford")
+        val colors = listOf("Green", "Pink", "Blue", "White", "Silver", "Red", "Black")
+
+        return List(count) { i ->
+            Auto(
+                model = models.random(),
+                brand = brands.random(),
+                number = i + 1,
+                year = (1885..2024).random(),
+                color = colors.random(),
+                maxSpeed = (50..533).random()
+            )
+        }
+    }
+
+    private fun startRace(cars: List<Auto>) {
+        var round = 1
+        var participants = cars.toMutableList()
+
+        while (participants.size > 1) {
+            println("--- Раунд $round ---")
+            val winners = mutableListOf<Auto>()
+
+            val pairs = participants.shuffled().chunked(2)
+            for (pair in pairs) {
+                if (pair.size == 2) {
+                    val car1 = pair[0]
+                    val car2 = pair[1]
+
+
+                    val winner = if (car1.maxSpeed > car2.maxSpeed) car1 else car2
+                    println("Гонка между ${car1.number} и ${car2.number}, Победитель ${winner.number}")
+                    winners.add(winner)
+                } else {
+                    println("${pair[0].number} - Нет пары, следующий круг")
+                    winners.add(pair[0])
+                }
+            }
+
+
+            participants = winners
+            round++
+        }
+
+        println("Победитель: ${participants[0].number}")
     }
 }
